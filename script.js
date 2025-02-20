4ca258996cfb018f3ce7bd916aef36ab2e299f46
isaacbis1 <isaacmorg93@gmail.com>
2025-02-20 22:56:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,18 +20,27 @@ document.addEventListener('keydown', (event) => {
  *  CONFIGURAZIONE FIREBASE
  *****************************************/
 // Usa i dati che hai postato, ad esempio:
-const firebaseConfig = {
- apiKey: "AIzaSyDYACM87SGSMgEb6hdG_08zufK7jvjf-rA",
-  authDomain: "nuovo-campi1.firebaseapp.com",
-  projectId: "nuovo-campi1",
-  storageBucket: "nuovo-campi1.firebasestorage.app",
-  messagingSenderId: "705263020150",
-  appId: "1:705263020150:web:dfc39025eb3813df231757",
-  measurementId: "G-YH6WS0TZZN"
-};
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCSc8mmc1Kpe5Zt6ecFN9X0UqhJkXdItt0",
+  authDomain: "campi-finale.firebaseapp.com",
+  projectId: "campi-finale",
+  storageBucket: "campi-finale.firebasestorage.app",
+  messagingSenderId: "591857565250",
+  appId: "1:591857565250:web:838d34afe090e840e377db",
+  measurementId: "G-K0FY33VL8N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 /*****************************************
  *  DIZIONARIO DI 200 user + ADMIN
  *****************************************/
